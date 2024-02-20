d3037c63f7ba00943434bc6811c503ac0b3e9ab5
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 17:27:41 +0600

    Updated

--- src/services/socketService/socketConnect.js
+++ src/services/socketService/socketConnect.js
@@ -2,21 +2,24 @@ import { useEffect } from "react";
 import { backURL } from "../../common/config";
 import io from "socket.io-client";
 
-const socketConnect = (jwt) => {
+const socketConnect = () => {
   // Remove trailing slash and query string from backURL
-
+  const jwt = localStorage.getItem("jwt");
   //   const socketURL = backURL.replace(/\/\?/, "");
 
   if (jwt) {
     // Initialize socket connection with jwtToken query parameter
     const socket = io(backURL, {
       query: { jwtToken: jwt },
-      //   transports: ["polling"],
+      transports: ["websocket"],
     });
 
     // Event listeners for error handling
     socket.on("connect_error", (error) => {
       console.error("Socket connection error:", error);
+    });
+    socket.emit("Connection_Time", () => {
+      setTimeout(() => {}, 5000);
     });
 
     socket.on("error", (error) => {

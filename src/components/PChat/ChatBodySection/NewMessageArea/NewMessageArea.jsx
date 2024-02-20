9e4aa442ed9654bc26c9d91cdb8132cbe9b25e15
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 18:51:53 +0600

    Updated

--- src/components/PChat/ChatBodySection/NewMessageArea/NewMessageArea.jsx
+++ src/components/PChat/ChatBodySection/NewMessageArea/NewMessageArea.jsx
@@ -38,7 +38,7 @@ const NewMessageArea = () => {
 
   const sendMessage = () => {
     console.log("Send Message Hit", message, chatId);
-    sockett.io("sendMessage", {
+    sockett.emit("sendMessage", {
       message: message,
       chatId: chatId,
     });

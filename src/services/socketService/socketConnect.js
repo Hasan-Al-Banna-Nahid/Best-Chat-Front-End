5cecfb2380c028869bcbd04561828ed90599e70f
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 15:21:56 +0600

    Updated

--- src/services/socketService/socketConnect.js
+++ src/services/socketService/socketConnect.js
@@ -1,16 +1,15 @@
 import { backURL } from "../../common/config";
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
 const socketConnect = (jwt) => {
-    // const socket = await io.connect(backURL)
-    if (jwt) {
-        const socket = io(backURL, {
-            query:{ jwtToken: jwt }
-        })
-        console.log('this is socketConnect----')
-        return socket
-    }
-    
+  // const socket = await io.connect(backURL)
+  if (jwt) {
+    const socket = io(backURL, {
+      query: { jwtToken: jwt },
+    });
+    console.log("this is socketConnect----");
+    return socket;
+  }
 };
 
 export default socketConnect;

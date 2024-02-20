5ab5d334c2ddc4af1a4e6c3c42e21063c58bef83
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 19:37:34 +0600

    Updated

--- src/services/socketService/sendDataOnConnection.js
+++ src/services/socketService/sendDataOnConnection.js
@@ -1,16 +1,16 @@
-// import { backURL } from "../../common/config";
+import { backURL } from "../../common/config";
 
-// const sendDataOnConnection = async (socket, jwt) => {
-//   console.log("-------this is sendDataOnConnection start----------");
-//   //   const jwt = localStorage.getItem("jwt");
-//   if (socket && jwt) {
-//     socket.on("connect", () => {
-//       socket.emit("sendDataOnConnection", {
-//         jwt: jwt,
-//       });
-//     });
-//     console.log("sendDataOnConnection done");
-//   }
-// };
+const sendDataOnConnection = async (socket, jwt) => {
+  console.log("-------this is sendDataOnConnection start----------");
+  //   const jwt = localStorage.getItem("jwt");
+  if (socket && jwt) {
+    socket.on("connect", () => {
+      socket.emit("sendDataOnConnection", {
+        jwt: jwt,
+      });
+    });
+    console.log("sendDataOnConnection done");
+  }
+};
 
-// export default sendDataOnConnection;
+export default sendDataOnConnection;

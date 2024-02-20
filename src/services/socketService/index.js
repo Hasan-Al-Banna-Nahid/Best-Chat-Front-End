04c922b01094de450893f93051007b43157f376c
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 15:00:23 +0600

    Updated

--- src/services/socketService/index.js
+++ src/services/socketService/index.js
@@ -3,13 +3,13 @@ import socketDisconnect from "./socketDisconnect";
 import sendDataOnConnection from "./sendDataOnConnection";
 import sendDataOnJoinRoom from "./sendDataOnJoinRoom";
 import sendDataOnLeftRoom from "./sendDataOnLeftRoom";
-import subscribeOnNewMessage from './subscribeOnNewMessage'
+import subscribeOnNewMessage from "./subscribeOnNewMessage";
 
 export const socketService = {
-    socketConnect: socketConnect,
-    // sendDataOnConnection: sendDataOnConnection,  
-    sendDataOnJoinRoom: sendDataOnJoinRoom,
-    sendDataOnLeftRoom: sendDataOnLeftRoom,
-    subscribeOnNewMessage: subscribeOnNewMessage,
-    socketDisconnect: socketDisconnect, 
+  socketConnect: socketConnect,
+  sendDataOnConnection: sendDataOnConnection,
+  sendDataOnJoinRoom: sendDataOnJoinRoom,
+  sendDataOnLeftRoom: sendDataOnLeftRoom,
+  subscribeOnNewMessage: subscribeOnNewMessage,
+  socketDisconnect: socketDisconnect,
 };

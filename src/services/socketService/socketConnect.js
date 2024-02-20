fffa66fb90cb65e3a3c125b4d0eec63d9b654863
Hasan-Al-Banna-Nahid <iamnahid591998@gmail.com>
2024-02-20 20:43:57 +0600

    Updated

--- src/services/socketService/socketConnect.js
+++ src/services/socketService/socketConnect.js
@@ -4,14 +4,14 @@ import io from "socket.io-client";
 
 const socketConnect = (jwt) => {
   // Remove trailing slash and query string from backURL
-  // const socketURL = backURL.replace(/^https?:\/\//i, "ws://");
+  const socketURL = backURL.replace(/^https?:\/\//i, "ws://");
 
   if (jwt) {
     // Initialize socket connection with jwtToken query parameter
-    const socket = io(backURL, {
+    const socket = io(socketURL, {
       //   transports: ["websocket"],
       query: { jwtToken: jwt },
-      // transports: ["websocket"],
+      transports: ["websocket"],
       timeout: 100000000,
     });
 
